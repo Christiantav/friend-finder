7d783c03305953613c7194739f8515d89fc5b596
Christiantav <chrisadrianotav@gmail.com>
2018-10-06 11:00:56 -0400

    friendly commit

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+var friends = [ {
+    "name": "Christian",
+    "photo": "https://media.licdn.com/dms/image/C4E03AQH6G9JPyeq-4g/profile-displayphoto-shrink_200_200/0?e=1543449600&v=beta&t=zJcMU8K3A9V-vO8ydKjNgMjXEUOClJxU_OyMxDi2ni4",
+    "scores": [5,4,5,3,5,3,4,5,2,5]
+}, {
+    "name": "Brianna",
+    "photo": "https://media.licdn.com/dms/image/C4D03AQG96R-zU6eiew/profile-displayphoto-shrink_800_800/0?e=1543449600&v=beta&t=jJ3vcQV7Ea8C-leOAlD0gXWaVlKX2y2H_E-6FBptDTU",
+    "scores": [4,3,3,2,3,3,1,2,3,3]
+}, {
+    "name": "Rachel",
+    "photo": "https://media.licdn.com/dms/image/C5603AQFkEtgEJhZplg/profile-displayphoto-shrink_800_800/0?e=1543449600&v=beta&t=VqDySjy8rvf3mMJez9RV7veaRMD32cv0v7u4FP9vCEk",
+    "scores": [5,3,5,5,5,2,4,5,5,5]
+}, {
+    "name": "Brian",
+    "photo": "https://media.licdn.com/dms/image/C4E03AQFsxffOyVEtkw/profile-displayphoto-shrink_800_800/0?e=1543449600&v=beta&t=G73e30pdVFFNMZ70_QI9Z9nFxXqylR3Brid_LPzVPyg",
+    "scores": [4,3,4,4,4,4,5,2,3,4]
+}, {
+    "name": "Nik",
+    "photo": "https://media.licdn.com/dms/image/C5603AQFZ36b9YPHFMg/profile-displayphoto-shrink_800_800/0?e=1543449600&v=beta&t=T7R2vLZt-H7EuII_Hc_BXG_m1JpgpFo8-E11X6CIskk",
+    "scores": [4,5,3,4,5,5,3,3,4,5]
+}, {
+    "name": "Vitaley",
+    "photo": "https://media.licdn.com/dms/image/C5103AQH7Mq75EImDgQ/profile-displayphoto-shrink_800_800/0?e=1543449600&v=beta&t=mf4aHfhmogDQUrpnoj7nXcQMaof94kTTwiUTi1vEaso",
+    "scores": [5,4,5,5,5,4,3,4,1,2]
+}, {
+    "name": "Zara",
+    "photo": "https://media.licdn.com/dms/image/C4D03AQGLlQXO90TmPQ/profile-displayphoto-shrink_800_800/0?e=1543449600&v=beta&t=ehOoZfwL1fZWPLp0-LR9NFi_F7Kc3zMNaBUhJ6hjYms",
+    "scores": [5,4,4,5,5,4,5,4,1,5]
+}, {
+    "name": "Mike ZZZZZZ",
+    "photo": "https://media.licdn.com/dms/image/C5603AQGFe34MAisCdg/profile-displayphoto-shrink_800_800/0?e=1543449600&v=beta&t=6RxcnwQjQF6tZhE0VK1SOw_Wo_CHeM9rX7k36GbsJO8",
+    "scores": [4,3,5,4,4,5,5,5,4,5]
+}, {
+    "name": "Jorge",
+    "photo": "https://media.licdn.com/dms/image/C4E03AQFDw8mGumr6Lw/profile-displayphoto-shrink_800_800/0?e=1543449600&v=beta&t=iyULQAGkdJPUwQVUFfW0fDhKwzxQPukVsSyTqpyLDcc",
+    "scores": [5,4,3,4,5,4,4,3,5,4]
+}];
+
+module.exports = friends;
